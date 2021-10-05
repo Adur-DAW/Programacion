64378a9d53cbacbc86458ff320f8f6013efc88c6
Adur Marques <adur.marques@gmail.com>
2021-10-05 20:02:14 +0200

    Tarea 1, 2 y Teoria

---
+++ Tareas/Tarea 1/hola_mundo/HolaMundo.java
@@ -0,0 +1,11 @@
+public class HolaMundo
+{
+    public static void main (String args[]) {
+        System.out.println("Módulo Profesional - PROGRAMACIÓN");
+        System.out.println("UNIDAD DE TRABAJO 01");
+        System.out.println("Introducción a la programación");
+        System.out.println("Adur Marques Herrero");
+        System.out.println("Altsasu, Navarra");
+        System.out.println("2021/09/29");
+    }
+}
